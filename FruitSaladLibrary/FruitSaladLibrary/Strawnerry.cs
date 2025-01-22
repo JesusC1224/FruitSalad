af893f27c197baeb4751fe075faeb133b65baca6
JesusC1224 <ydjkings@gmail.com>
2025-01-21 19:41:01 -0500

    Completed FruitSalad Example
    Example of OOP inheritance and an abstraction

---
+++ FruitSaladLibrary/FruitSaladLibrary/Strawnerry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSaladLibrary
+{
+    public class Strawberry : Fruit
+    {
+        public bool IsItGoodForJam { get; set; }
+        public int SweetnessLevel { get; set; }
+        public Strawberry(string color, double weight, bool isOrganic, bool isItGoodForJam, int sweetnessLevel) : base("Strawberry", color, weight, isOrganic) {
+            SweetnessLevel = sweetnessLevel;
+            IsItGoodForJam = isItGoodForJam;
+        }
+        //implementation of the abstract desbribe method
+        public override string Describe()
+        {
+            String jamLabal = IsItGoodForJam ? "Good for jam!" : "Not ideal for jam";
+            return $"{base.ToString()}, Sweetness Level:  {SweetnessLevel}, : {jamLabal}";
+        }
+    }
+}
